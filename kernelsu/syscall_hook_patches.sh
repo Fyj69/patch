615bba23f9488b41bee7636a639a1420e9b35418
Fyj69 <18164452+Fyj69@users.noreply.github.com>
2025-07-22 15:16:21 +0800

    Update syscall_hook_patches.sh

--- kernelsu/syscall_hook_patches.sh
+++ kernelsu/syscall_hook_patches.sh
@@ -10,8 +10,10 @@ patch_files=(
     fs/open.c
     fs/read_write.c
     fs/stat.c
-    fs/namei.c
+    fs/namespace.c
+    fs/devpts/inode.c
     drivers/input/input.c
+    drivers/tty/pty.c
     security/security.c
     security/selinux/hooks.c
 )
@@ -35,62 +37,64 @@ for i in "${patch_files[@]}"; do
     # arch/arm/kernel/ changes
     ## sys_arm.c
     arch/arm/kernel/sys_arm.c)
-        if [ "$FIRST_VERSION" -lt 4 ] && [ "$SECOND_VERSION" -lt 5 ]; then
-            sed -i '/asmlinkage int sys_execve(const char __user \*filenamei,/i \#ifdef CONFIG_KSU\nextern int __attribute__((hot)) ksu_handle_execve_sucompat(int \*fd,\n\t\t\t\tconst char __user \*\*filename_user,\n\t\t\t\tvoid \*__never_use_argv, void \*__never_use_envp,\n\t\t\t\tint \*__never_use_flags);\n#endif' arch/arm/kernel/sys_arm.c
-            sed -i '/filename = getname(filenamei);/i \#ifdef CONFIG_KSU\n\tksu_handle_execve_sucompat((int \*)AT_FDCWD, &filenamei, NULL, NULL, NULL);\n#endif' arch/arm/kernel/sys_arm.c
+        if [ "$FIRST_VERSION" -lt 4 ] && [ "$SECOND_VERSION" -lt 18 ]; then
+            sed -i '/asmlinkage int sys_execve(const char __user \*filenamei,/i \#ifdef CONFIG_KSU\nextern bool ksu_execveat_hook __read_mostly;\nextern int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,\n\t\t\t\t        void *__never_use_argv, void *__never_use_envp,\n\t\t\t\t        int *__never_use_flags);\nextern int ksu_handle_execve_ksud(const char __user *filename_user,\n\t\t\tconst char __user *const __user *__argv);\n#endif' arch/arm/kernel/sys_arm.c
+            sed -i '/error = PTR_ERR(filename);/a \#ifdef CONFIG_KSU\n\tif (unlikely(ksu_execveat_hook))\n\t\tksu_handle_execve_ksud(filename, argv);\n\telse\n\t\tksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);\n#endif' arch/arm/kernel/sys_arm.c
         fi
         ;;
 
     # fs/ changes
     ## exec.c
     fs/exec.c)
-        if [ "$FIRST_VERSION" -lt 4 ] && [ "$SECOND_VERSION" -lt 11 ]; then
-            sed -i '/SYSCALL_DEFINE3(execve,/i \#ifdef CONFIG_KSU\nextern __attribute__((hot)) int ksu_handle_execve_sucompat(int \*fd,\n\t\t\t\tconst char __user \*\*filename_user,\n\t\t\t\tvoid \*__never_use_argv,\n\t\t\t\tvoid \*__never_use_envp,\n\t\t\t\tint \*__never_use_flags);\n#endif' fs/exec.c
-            sed -i '/struct filename \*path = getname(filename);/i \#ifdef CONFIG_KSU\n\tksu_handle_execve_sucompat((int \*)AT_FDCWD, &filename, NULL, NULL, NULL);\n#endif' fs/exec.c
-        else
-            sed -i '/SYSCALL_DEFINE3(execve,/i \#ifdef CONFIG_KSU\nextern __attribute__((hot)) int ksu_handle_execve_sucompat(int \*fd,\n\t\t\t       const char __user \*\*filename_user,\n\t\t\t       void \*__never_use_argv, void \*__never_use_envp,\n\t\t\t       int \*__never_use_flags);\n#endif' fs/exec.c
-            sed -i '/return do_execve(getname(filename), argv, envp);/i \#ifdef CONFIG_KSU\n\tksu_handle_execve_sucompat((int \*)AT_FDCWD, &filename, NULL, NULL, NULL);\n#endif' fs/exec.c
-            sed -i '/return compat_do_execve(getname(filename), argv, envp);/i \#ifdef CONFIG_KSU\n\tksu_handle_execve_sucompat((int \*)AT_FDCWD, &filename, NULL, NULL, NULL);\n#endif' fs/exec.c
-        fi
+        sed -i '0,/SYSCALL_DEFINE3(execve,/ {
+                                                      /SYSCALL_DEFINE3(execve,/i \
+                                                  #ifdef CONFIG_KSU\nextern bool ksu_execveat_hook __read_mostly;\nextern int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,\n\t\t   void *__never_use_argv, void *__never_use_envp,\n\t\t   int *__never_use_flags);\nextern int ksu_handle_execve_ksud(const char __user *filename_user,\n\t\tconst char __user *const __user *__argv);\n#ifdef CONFIG_COMPAT  \/\/ 32-on-64 support\nextern int ksu_handle_compat_execve_ksud(const char __user *filename_user,\n\t\tconst compat_uptr_t __user *__argv);\n#endif\n#endif
+                                                  }' fs/exec.c
+
+        sed -i '/do_execve *(/,/^}/ {
+/struct user_arg_ptr envp = { .ptr.native = __envp };/a\
+#ifdef CONFIG_KSU\
+\tif (unlikely(ksu_execveat_hook))\
+\t\tksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);\
+\telse\
+\t\tksu_handle_execveat_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);\
+#endif
+}' fs/exec.c
+        sed -i ':a;N;$!ba;s/\(return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);\)/\n#ifdef CONFIG_KSU\n\tif (!ksu_execveat_hook)\n\t\tksu_handle_execveat_sucompat((int *)AT_FDCWD, \&filename, NULL, NULL, NULL); \/* 32-bit su *\/\n#endif\n\1/2' fs/exec.c
         ;;
 
     ## open.c
     fs/open.c)
-        if [ "$FIRST_VERSION" -lt 5 ] && [ "$SECOND_VERSION" -lt 19 ]; then
-            sed -i '/SYSCALL_DEFINE3(faccessat, int, dfd, const char __user \*, filename, int, mode)/i \#ifdef CONFIG_KSU\nextern int ksu_handle_faccessat(int \*dfd, const char __user \*\*filename_user, int \*mode,\n\t\t\t                     int \*flags);\n#endif' fs/open.c
+        if grep -q "return do_faccessat(dfd, filename, mode);" fs/open.c; then
+            sed -i '/return do_faccessat(dfd, filename, mode);/i\#ifdef CONFIG_KSU\nextern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,\n\tint *flags);\n#endif' fs/open.c
+        elif [ "$FIRST_VERSION" -lt 4 ] && [ "$SECOND_VERSION" -lt 18 ]; then
             sed -i '/if (mode & ~S_IRWXO)/i \#ifdef CONFIG_KSU\n\tksu_handle_faccessat(&dfd, &filename, &mode, NULL);\n#endif' fs/open.c
         else
-            sed -i '/SYSCALL_DEFINE3(faccessat, int, dfd, const char __user \*, filename, int, mode)/i \#ifdef CONFIG_KSU\nextern __attribute__((hot)) int ksu_handle_faccessat(int \*dfd, \n\t\t\t                    const char __user \*\*filename_user, int \*mode, int \*flags);\n#endif' fs/open.c
-            sed -i '/return do_faccessat(dfd, filename, mode);/i \#ifdef CONFIG_KSU\n\tksu_handle_faccessat(&dfd, &filename, &mode, NULL);\n#endif' fs/open.c
+            sed -i ':a;N;$!ba;s/\(unsigned int lookup_flags = LOOKUP_FOLLOW;\)/\1\n#ifdef CONFIG_KSU\n\tksu_handle_faccessat(\&dfd, \&filename, \&mode, NULL);\n#endif/2' fs/open.c
+
         fi
+        sed -i '0,/SYSCALL_DEFINE3(faccessat, int, dfd, const char __user \*, filename, int, mode)/s//#ifdef CONFIG_KSU\nextern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,\n\t\t\t                    int *flags);\n#endif\n&/' fs/open.c
         ;;
 
     ## read_write.c
     fs/read_write.c)
-        if [ "$FIRST_VERSION" -lt 5 ] && [ "$SECOND_VERSION" -lt 19 ]; then
-            sed -i '/SYSCALL_DEFINE3(read, unsigned int, fd, char __user \*, buf, size_t, count)/i \#ifdef CONFIG_KSU\nextern bool ksu_vfs_read_hook __read_mostly;\nextern int ksu_handle_sys_read(unsigned int fd, char __user \*\*buf_ptr,\n\t\t\tsize_t \*count_ptr);\n#endif' fs/read_write.c
-            sed -i '0,/if (f.file) {/s//if (f.file) {\nloff_t pos;\n#ifdef CONFIG_KSU\n\tif (unlikely(ksu_vfs_read_hook))\n\t\tksu_handle_sys_read(fd, \&buf, \&count);\n#endif/' fs/read_write.c
-	          sed -i '0,/loff_t pos = file_pos_read(f.file);/s/loff_t pos = file_pos_read(f.file);/pos = file_pos_read(f.file);/' fs/read_write.c
-        else
-            sed -i '/SYSCALL_DEFINE3(read, unsigned int, fd, char __user \*, buf, size_t, count)/i\#ifdef CONFIG_KSU\nextern bool ksu_vfs_read_hook __read_mostly;\nextern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,\n\t\t\tsize_t *count_ptr);\n#endif' fs/read_write.c
+        if grep -q "return ksys_read(fd, buf, count);" fs/read_write.c; then
             sed -i '/return ksys_read(fd, buf, count);/i\#ifdef CONFIG_KSU\n\tif (unlikely(ksu_vfs_read_hook))\n\t\tksu_handle_sys_read(fd, &buf, &count);\n#endif' fs/read_write.c
+        elif [ "$FIRST_VERSION" -lt 4 ] && [ "$SECOND_VERSION" -lt 18 ]; then
+            sed -i '0,/ret = vfs_read(file, buf, count, &pos);/ { /ret = vfs_read(file, buf, count, &pos);/i \#ifdef CONFIG_KSU\n\tif (unlikely(ksu_vfs_read_hook))\n\t\tksu_handle_sys_read(fd, &buf, &count);\n#endif
+                                                           }' fs/read_write.c
+        else
+            sed -i '0,/if (f.file) {/s//if (f.file) {\nloff_t pos;\n#ifdef CONFIG_KSU\n\tif (unlikely(ksu_vfs_read_hook))\n\t\tksu_handle_sys_read(fd, \&buf, \&count);\n#endif/' fs/read_write.c
+	    sed -i '0,/loff_t pos = file_pos_read(f.file);/s/loff_t pos = file_pos_read(f.file);/pos = file_pos_read(f.file);/' fs/read_write.c
         fi
+        sed -i '/SYSCALL_DEFINE3(read, unsigned int, fd, char __user \*, buf, size_t, count)/i\#ifdef CONFIG_KSU\nextern bool ksu_vfs_read_hook __read_mostly;\nextern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,\n\t\t\tsize_t *count_ptr);\n#endif' fs/read_write.c
         ;;
 
     ## stat.c
     fs/stat.c)
-        sed -i '/#if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)/i \#ifdef CONFIG_KSU\nextern __attribute__((hot)) int ksu_handle_stat(int \*dfd, \n\t\t\t                    const char __user \*\*filename_user, int \*flags);\n#endif' fs/stat.c
+        sed -i '/#if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)/i\#ifdef CONFIG_KSU\nextern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);\n#endif' fs/stat.c
         sed -i '0,/\terror = vfs_fstatat(dfd, filename, &stat, flag);/s//#ifdef CONFIG_KSU\n\tksu_handle_stat(\&dfd, \&filename, \&flag);\n#endif\n&/' fs/stat.c
         sed -i ':a;N;$!ba;s/\(\terror = vfs_fstatat(dfd, filename, &stat, flag);\)/#ifdef CONFIG_KSU\n\tksu_handle_stat(\&dfd, \&filename, \&flag);\n#endif\n\1/2' fs/stat.c
-        ;;
-
-    ## namei.c
-    fs/namei.c)
-        if [ "$FIRST_VERSION" -lt 4 ] && [ "$SECOND_VERSION" -lt 5 ]; then
-            sed -i '/err = lookup_slow(nd, name, path);/c \ \t\tif (strstr(current->comm, "throne_tracker") == NULL)\n\t\t\terr = lookup_slow(nd, name, path);\n\t\telse\n\t\t\terr = -ENOENT;\n' fs/namei.c
-        elif [ "$FIRST_VERSION" -lt 4 ] && [ "$SECOND_VERSION" -lt 19 ]; then
-            sed -i '/if (unlikely(err)) {/a \#ifdef CONFIG_KSU\n\t\tif (unlikely(strstr(current->comm, "throne_tracker"))) {\n\t\t\terr = -ENOENT;\n\t\t\tgoto out_err;\n\t\t}\n#endif' fs/namei.c
-        fi
         ;;
 
     # drivers/input changes
@@ -121,6 +125,75 @@ for i in "${patch_files[@]}"; do
             sed -i '/rc = security_bounded_transition(old_tsec->sid, new_tsec->sid);/i \#ifdef CONFIG_KSU\n\tif (is_ksu_transition(old_tsec, new_tsec))\n\t\treturn 0;\n#endif' security/selinux/hooks.c
         fi
         ;;
+
+    # pty.c
+    drivers/tty/pty.c)
+        sed -i '0,/static struct tty_struct \*pts_unix98_lookup(struct tty_driver \*driver,/s//#ifdef CONFIG_KSU\nextern int ksu_handle_devpts(struct inode*);\n#endif\n&/' drivers/tty/pty.c
+        sed -i ':a;N;$!ba;s/\(\tmutex_lock(&devpts_mutex);\)/#ifdef CONFIG_KSU\n\tksu_handle_devpts((struct inode *)file->f_path.dentry->d_inode);\n#endif\n\1/2' drivers/tty/pty.c
+        ;;
+
+    # fs/ changes
+    ## fs/namespace.c
+    fs/namespace.c)
+        if [[ $(grep -c "static int can_umount(const struct" fs/namespace.c) == 0 ]]; then
+            if grep -q "may_mandlock(void)" fs/namespace.c; then
+                umount='may_mandlock(void)/,/^}/ { /^}/ {n;a'
+            else
+                umount='int ksys_umount(char __user \*name, int flags)/i'
+            fi
+        sed -i "/${umount} \
+#ifdef CONFIG_KSU\n\
+static int can_umount(const struct path *path, int flags)\n\
+{\n\
+    struct mount *mnt = real_mount(path->mnt);\n\
+\n\
+    if (flags & ~(MNT_FORCE | MNT_DETACH | MNT_EXPIRE | UMOUNT_NOFOLLOW))\n\
+        return -EINVAL;\n\
+    if (!may_mount())\n\
+        return -EPERM;\n\
+    if (path->dentry != path->mnt->mnt_root)\n\
+        return -EINVAL;\n\
+    if (!check_mnt(mnt))\n\
+        return -EINVAL;\n\
+    if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */\n\
+        return -EINVAL;\n\
+    if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))\n\
+        return -EPERM;\n\
+    return 0;\n\
+}\n\
+\n\
+int path_umount(struct path *path, int flags)\n\
+{\n\
+    struct mount *mnt = real_mount(path->mnt);\n\
+    int ret;\n\
+\n\
+    ret = can_umount(path, flags);\n\
+    if (!ret)\n\
+        ret = do_umount(mnt, flags);\n\
+\n\
+    /* we must not call path_put() as that would clear mnt_expiry_mark */\n\
+    dput(path->dentry);\n\
+    mntput_no_expire(mnt);\n\
+    return ret;\n\
+}\n\
+#endif
+}}" fs/namespace.c
+        fi
+        ;;
+
+    # fs/devpts changes
+    ## inode.c
+    fs/devpts/inode.c)
+        sed -i '/struct dentry \*devpts_pty_new/,/return dentry;/ {
+    /return dentry;/ {n; a\
+#ifdef CONFIG_KSU\nextern int ksu_handle_devpts(struct inode*);\n#endif
+    }
+}
+        /if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)/i\
+	#ifdef CONFIG_KSU\n	ksu_handle_devpts(dentry->d_inode);\n	#endif' fs/devpts/inode.c
+        ;;
     esac
+
+    echo "Patch applied successfully to $i"
 
 done
